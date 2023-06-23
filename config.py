d7a36ba638cbe6c62cbcb856c254fa1d123a0830
vasilii2501 <119878438+vasilii2501@users.noreply.github.com>
2023-06-23 23:35:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-comunity_token = 'vk1.a.9lTUQqNj2Lr2LrGnuLPhjCfGnvA9l_gUpJTWgimkHewc3Nx47Sei3XDM5eq41x58vhRdp0F5HExzIGAYxr59FPIgAO9FSYQWbDQDyFmarlMmVKJ6O1qdqeSu1I3t-KCMb6kqgQc0EjbbpMBkhnPAoRV4JcQ5PzBedFbWRBts48bzxj4WWtWpO-hNWwLpJRj0L5jCoa80Bkrq697kiyDXhA'
-acces_token = 'vk1.a.yPfRrZCwDR7bASIcLAJVcEGASEj9SGstxI27RIHcowC0cby0PvN8_grfjxHgiXOZm6O5OwThpJmhLVFRE9WyIVVNflgSk-lSdLGTyPD2BZPnoOIUc5uyrufhJIS9yrek77pIPy0slGR4IjJlowISbpPyV-TkiM2ufpkZ7ek47WXwPjFbItmTdLtVaKX8o-I-'
-db_url_object = 'postgresql://postgres:Xthngjlthb27!@localhost:5432/student_diplom'
+comunity_token = ''
+acces_token = ''
+db_url_object = 'postgresql://postgres:@localhost:5432/student_diplom'
